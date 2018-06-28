7837cd094d29930fe408f0d4d6abd808af74d8c3
ab.mcl <ab.mcl@metrocast.net>
2018-06-28 15:36:47 -0400

    Updated, css connected

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const exhbs = require('express-handlebars');
 const mysql = require('mysql');
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+//app.use(express.static(__dirname + "/public/"));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded( {
 		extended: false
 	}));
@@ -18,3 +19,4 @@ app.use('/', route);
 var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('You are listening on port: ' + port);
+
